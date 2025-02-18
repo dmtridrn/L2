15f74c61690de0b04a88c9da7cd70339b62f0f03
Dimitri Dron <dimitri.dron-jeannequin@etu.u-paris.fr>
2025-02-18 20:18:00 +0100

    ex4

--- langage c/tp4/ex4.c
+++ langage c/tp4/ex4.c
@@ -1,3 +1,25 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <stdbool.h>
+
+void print_tab1(int *t, size_t start, size_t end){
+    for(int i = start; i < end; i++){
+        int *adr = &t[i];
+        int val = t[i];
+        printf("%p : %i\n", adr, val);
+    }
+}
+
+void print_tab2(int *pstart, int *pend){
+    for(int* i = pstart; i < pend; i++){
+        int *adr = i;
+        int val = *i;
+        printf("%p : %i\n", adr, val);
+    }
+}
+
+int main(){
+    int tab[] = {9,3,6,1,5,2};
+    //print_tab1(tab, 0, 6);
+    print_tab2(tab+2, tab + 6);
+}
