0c423ea8863f92d42f40db05e7c9731e60512e0a
Dron Dimitri <dimitri.dron-jeannequin@etu.u-paris.fr>
2025-02-18 12:15:36 +0100

    a

--- algo2/td4/exo2.py
+++ algo2/td4/exo2.py
@@ -5,6 +5,25 @@ def occurence_naif(tab, x):
             cpt += 1
     return cpt
 
-tab = [1,5,6,9,8,5,4,7,4,5,2,3,6,9,8,5]
-print(occurence_naif(tab, 2))
+def trouvePremier(tab, x, debut=0, fin=None):
+    if fin is None:
+        fin = len(tab) - 1
+    while debut <= fin:
+        mid = (debut + fin) // 2
+        if (mid == 0 or tab[mid-1] < x) and tab[mid] == x:
+            return mid
+        elif tab[mid] < x:
+            debut = mid + 1
+        else:
+            fin = mid - 1
+    return None
+    
+
+tab = [1,3,5,5,5,5,8,9,12,16,19,26,61]
+
+
+print(trouvePremier(tab, 5))
+print(trouvePremier(tab, 2))
+print(trouvePremier(tab, 1))
+print(trouvePremier(tab, 61))
     
